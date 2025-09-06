c28fedaefcdd501f6650b2aaff67e05bbb99ff70
mugdA001 <mugdarat.p@ku.th>
2025-09-06 21:06:17 +0700

    Add files via upload

--- Lab10/ManualTestRunner.java
+++ Lab10/ManualTestRunner.java
@@ -24,9 +24,40 @@ public class ManualTestRunner {
         orderProcessor.register(inventory);
         orderProcessor.register(emailler);
 
-        System.err.println("\n--- 2.Testing Strategy Pattern ---");
+        System.err.println("\n--- 2.Testing Strategy Pattern (Discounts) ---");
         double originalPrice = myOrder.getTotalPrice();
         System.out.println("Originai Price: " + originalPrice);
+
+        DiscountStrategy tenPercentOff = new PercentageDiscount(10);
+        double priceAfterPercentage = calculator.calculateFinalPrice(myOrder, tenPercentOff);
+        System.out.println("Price with 10% discount: " + priceAfterPercentage);
+
+        DiscountStrategy fiveHundredOff = new FixedDiscount(500);
+        double priceAfterFixed = calculator.calculateFinalPrice(myOrder, fiveHundredOff);
+        System.out.println("Price with 500 THB discount: " + priceAfterFixed);
+
+        System.out.println("\n--- 3.Testing Factory and Decorator Patterns (Shipment) --- ");
+        // สร้างการจัดส่งแบบมาตรฐาน
+        Shipment standardShipment = shipmentFactory.createShipment("STANDARD");
+        System.out.println("Base Shipment: " + standardShipment.getInfo() + ", Cost: " + standardShipment.getCost());
+
+        // "ห่อ" ด้วยบริการห่อของขวัญ
+        Shipment giftWrapped = new GiftWrapDecorator(standardShipment);
+        System.out.println("Decorated: " + giftWrapped.getInfo() + ", Cost: " + giftWrapped.getCost());
+
+        // "ห่อ" ทับด้วยบริการประกันสินค้า
+        Shipment fullyLoaded = new InsuranceDecorator(giftWrapped, myOrder);
+        System.out.println("Fully Decorated: " + fullyLoaded.getInfo() + ", Cost: " + fullyLoaded.getCost());
+
+        System.out.println("\n--- 4. Printing Final Summary ---");
+        double finalPrice = priceAfterPercentage; // สมมติว่าใช้ส่วนลด 10%
+        double totalCost = finalPrice + fullyLoaded.getCost();
+        System.out.println("Final price after discount: " + finalPrice);
+        System.out.println("Final Shipment cost: " + fullyLoaded.getCost());
+        System.out.println("TOTAL TO PAY: " + totalCost);
+
+        // --- 5. Testing Observer Pattern (Processing Order)
+        orderProcessor.processOder(myOrder);
         
     }
 }
